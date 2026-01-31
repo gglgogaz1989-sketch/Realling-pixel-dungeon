daf8279f0536b728caec4e30c4fda9ddb7ecc061
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 15:22:37 +0200

    Update ShirdSprite.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ShirdSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ShirdSprite.java
@@ -1,32 +1,24 @@
 package com.shatteredpixel.shatteredpixeldungeon.sprites;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
-import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Shird;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 
 public class ShirdSprite extends MobSprite {
-
-    @Override
-    public void link(Mob mob) {
-        super.link(mob);
-        // По умолчанию ставим спящую текстуру
-        texture = Assets.SHIRD_SLEEP; 
-    }
 
     @Override
     public void update() {
         super.update();
         if (ch == null) return;
 
-        // Логика смены картинок по стадиям ХП
-        if (ch.HP <= 0) {
-             // Текстура смерти (можно использовать idle)
-        } else if (((Shird)ch).isCasting) {
-            texture = Assets.SHIRD_CAST; // Стадия призыва
-        } else if (ch.state == ch.WANDERING || ch.state == ch.HUNTING) {
-            texture = Assets.SHIRD_IDLE; // Проснулся и стоит/ходит
+        // Переключение стадий
+        if (ch.HP <= 500 && ch.HP > 100) {
+            texture = Assets.SHIRD_ATTACK; // Стадия лазера
+        } else if (ch.HP <= 750) {
+            texture = Assets.SHIRD_CAST;   // Стадия призыва
+        } else if (ch.state != ch.SLEEPING) {
+            texture = Assets.SHIRD_IDLE;   // Проснулся
         } else {
-            texture = Assets.SHIRD_SLEEP; // Спит
+            texture = Assets.SHIRD_SLEEP;  // Спит
         }
     }
-    }
+                }
