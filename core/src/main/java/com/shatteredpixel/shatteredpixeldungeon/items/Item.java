03dd0868209d482a3d17f16181989c635a948386
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 14:06:04 +0200

    Update Item.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Item.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Item.java
@@ -21,6 +21,7 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Badges;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
