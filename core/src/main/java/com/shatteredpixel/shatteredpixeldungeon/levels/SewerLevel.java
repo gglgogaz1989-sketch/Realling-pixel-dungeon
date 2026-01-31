40f9a41585b2273d1b19bc851319d1f16db113c0
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 20:23:11 +0200

    Update SewerLevel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
@@ -2,59 +2,82 @@ package com.shatteredpixel.shatteredpixeldungeon.levels;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
-import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Shird;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Ghost;
+import com.shatteredpixel.shatteredpixeldungeon.items.Amulet;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.SewerPainter;
-import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Ghost;
+import com.shatteredpixel.shatteredpixeldungeon.levels.traps.WornDartTrap;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.watabou.utils.Random;
 
 public class SewerLevel extends RegularLevel {
 
-    // Музыка для SewerBossLevel и обычных уровней
-    public static final String[] SEWER_TRACK_LIST
-            = new String[]{Assets.Music.SEWERS_1, Assets.Music.SEWERS_2, Assets.Music.SEWERS_2,
-            Assets.Music.SEWERS_1, Assets.Music.SEWERS_3, Assets.Music.SEWERS_3};
-    public static final float[] SEWER_TRACK_CHANCES = new float[]{1f, 1f, 0.5f, 0.25f, 1f, 0.5f};
+	// Музыкальные треки для канализации
+	public static final String[] SEWER_TRACK_LIST = new String[]{
+		Assets.Music.SEWERS_1, Assets.Music.SEWERS_2, Assets.Music.SEWERS_3
+	};
+	public static final float[] SEWER_TRACK_CHANCES = new float[]{1f, 1f, 1f};
 
-    {
-        color1 = 0x48763c;
-        color2 = 0x59994a;
-    }
+	{
+		color1 = 0x48763c;
+		color2 = 0x59994a;
+	}
 
-    // ТУТ ГЕНЕРИРУЮТСЯ ВРАГИ
-    @Override
-    protected void createMobs() {
-        // Спавним призрака (квест)
-        Ghost.Quest.spawn( this, roomExit );
-        // super.createMobs() запускает генерацию обычных крыс и гноллов
-        super.createMobs();
+	@Override
+	public String tilesTex() {
+		return Assets.Environment.TILES_SEWERS;
+	}
 
-        // Твоя лопата прямо на входе
-        if (Dungeon.depth == 1) {
-            Dungeon.level.drop(new Shovel(), Dungeon.hero.pos).sprite.drop();
-        }
-    }
+	@Override
+	public String waterTex() {
+		return Assets.Environment.WATER_SEWERS;
+	}
 
-    // ТУТ ГЕНЕРИРУЮТСЯ ПРЕДМЕТЫ И СУНДУКИ
-    @Override
-    protected void createItems() {
-        // Это вызывает стандартную генерацию предметов и сундуков из RegularLevel
-        super.createItems();
-    }
+	@Override
+	protected Painter painter() {
+		return new SewerPainter();
+	}
 
-    @Override
-    protected Painter painter() {
-        // SewerPainter отвечает за расстановку дверей, травы и воды
-        return new SewerPainter();
-    }
+	@Override
+	protected void createMobs() {
+		// 1. Спавним квестового призрака
+		Ghost.Quest.spawn( this, roomExit );
+		
+		// 2. Генерируем стандартных врагов (крыс, гноллов)
+		super.createMobs();
 
-    @Override
-    public String tilesTex() {
-        return Assets.Environment.TILES_SEWERS;
-    }
+		// 3. Твоя лопата (спавним прямо под героем на 1 этаже)
+		if (Dungeon.depth == 1 && Dungeon.hero != null) {
+			Dungeon.level.drop(new Shovel(), Dungeon.hero.pos).sprite.drop();
+		}
+	}
 
-    @Override
-    public String waterTex() {
-        return Assets.Environment.WATER_SEWERS;
-    }
+	@Override
+	protected void createItems() {
+		// Этот вызов ГЕНЕРИРУЕТ СУНДУКИ, золото и расходники на полу
+		super.createItems();
+	}
+
+	@Override
+	protected int standardRooms(boolean forceMax) {
+		return 5 + (forceMax ? 1 : Random.Int(2));
+	}
+
+	@Override
+	protected Class<?>[] trapClasses() {
+		return new Class<?>[]{ WornDartTrap.class };
+	}
+
+	@Override
+	protected float[] trapChances() {
+		return new float[]{ 1f };
+	}
+
+	@Override
+	public String tileName( int tile ) {
+		if (tile == Terrain.WATER) return Messages.get(SewerLevel.class, "water_name");
+		return super.tileName( tile );
+	}
 }
