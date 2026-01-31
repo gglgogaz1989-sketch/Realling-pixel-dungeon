5097041970d25652c15a83de477dbe4df26ac90a
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 18:14:59 +0200

    Update Shovel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
@@ -2,15 +2,18 @@ package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
-import com.shatteredpixel.shatteredpixeldungeon.items.weapons.MeleeWeapon; // Добавили 's'
 import com.shatteredpixel.shatteredpixeldungeon.utils.Random;
 
+// Пробуем импортировать всё из папок, где может быть MeleeWeapon
+import com.shatteredpixel.shatteredpixeldungeon.items.*;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapons.*;
+
 public class Shovel extends MeleeWeapon {
-    {
-        name = "Лопата";
-    }
 
     public Shovel() {
+        name = "Лопата";
+        // Если на эту строку будет ругаться, значит в твоей версии 
+        // метод называется по-другому, но пока оставим так:
         initValues(5, 12, 1.0f, 1.0f); 
     }
 
