caa1999b5a9a720d90a77b4d27cbfcd0ba835a09
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 18:38:22 +0200

    Update Shovel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
@@ -2,20 +2,19 @@ package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
-// Исправляем импорт: MeleeWeapon лежит в папке items!
 import com.shatteredpixel.shatteredpixeldungeon.items.MeleeWeapon;
 
 public class Shovel extends MeleeWeapon {
 
     public Shovel() {
         name = "Лопата";
+        // Если initValues выдает ошибку, проверь параметры в MeleeWeapon.java
         initValues(5, 12, 1.0f, 1.0f); 
     }
 
     @Override
     public void proc(Char attacker, Char defender, int damage) {
         super.proc(attacker, defender, damage);
-        // Используем Math.random(), чтобы не зависеть от внешних классов Random
         if (Math.random() < 0.05) {
             Paralysis.affect(defender, 5f);
         }
