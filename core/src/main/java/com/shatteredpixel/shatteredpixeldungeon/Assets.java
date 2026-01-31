1a371e5944e962465cee81fc2cd76157954801ac
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 16:31:18 +0200

    Update Assets.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -243,7 +243,7 @@ public class Assets {
 		};
 	}
 
-	public static class Splashes:
+	public static class Splashes {
 		public static final String WARRIOR  = "splashes/warrior.jpg";
 		public static final String MAGE     = "splashes/mage.jpg";
 		public static final String ROGUE    = "splashes/rogue.jpg";
