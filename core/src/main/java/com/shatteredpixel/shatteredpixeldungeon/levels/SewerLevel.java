7733f9c610c1f33952a35fdd13afeb9a6b385ead
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 20:14:47 +0200

    Update SewerLevel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
@@ -1,45 +1,60 @@
 package com.shatteredpixel.shatteredpixeldungeon.levels;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Shird;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.SewerPainter;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Ghost;
 
 public class SewerLevel extends RegularLevel {
 
-	{
-		color1 = 0x48763c;
-		color2 = 0x59994a;
-	}
+    // Музыка для SewerBossLevel и обычных уровней
+    public static final String[] SEWER_TRACK_LIST
+            = new String[]{Assets.Music.SEWERS_1, Assets.Music.SEWERS_2, Assets.Music.SEWERS_2,
+            Assets.Music.SEWERS_1, Assets.Music.SEWERS_3, Assets.Music.SEWERS_3};
+    public static final float[] SEWER_TRACK_CHANCES = new float[]{1f, 1f, 0.5f, 0.25f, 1f, 0.5f};
 
-	@Override
-	protected void createMobs() {
-		super.createMobs();
+    {
+        color1 = 0x48763c;
+        color2 = 0x59994a;
+    }
 
-		// Спавним лопату прямо под героем на 1 этаже
-		if (Dungeon.depth == 1) {
-			Dungeon.level.drop(new Shovel(), Dungeon.hero.pos).sprite.drop();
-			
-			// Если всё же нужен босс, раскомментируйте строки ниже:
-			// Shird boss = new Shird();
-			// boss.pos = randomRespawnCell(boss);
-			// if (boss.pos != -1) Dungeon.level.spawnMob(boss);
-		}
-	}
+    // ТУТ ГЕНЕРИРУЮТСЯ ВРАГИ
+    @Override
+    protected void createMobs() {
+        // Спавним призрака (квест)
+        Ghost.Quest.spawn( this, roomExit );
+        // super.createMobs() запускает генерацию обычных крыс и гноллов
+        super.createMobs();
 
-	@Override
-	protected Painter painter() {
-		return new SewerPainter();
-	}
+        // Твоя лопата прямо на входе
+        if (Dungeon.depth == 1) {
+            Dungeon.level.drop(new Shovel(), Dungeon.hero.pos).sprite.drop();
+        }
+    }
 
-	@Override
-	public String tilesTex() {
-		return com.shatteredpixel.shatteredpixeldungeon.Assets.Environment.TILES_SEWERS;
-	}
+    // ТУТ ГЕНЕРИРУЮТСЯ ПРЕДМЕТЫ И СУНДУКИ
+    @Override
+    protected void createItems() {
+        // Это вызывает стандартную генерацию предметов и сундуков из RegularLevel
+        super.createItems();
+    }
 
-	@Override
-	public String waterTex() {
-		return com.shatteredpixel.shatteredpixeldungeon.Assets.Environment.WATER_SEWERS;
-	}
+    @Override
+    protected Painter painter() {
+        // SewerPainter отвечает за расстановку дверей, травы и воды
+        return new SewerPainter();
+    }
+
+    @Override
+    public String tilesTex() {
+        return Assets.Environment.TILES_SEWERS;
+    }
+
+    @Override
+    public String waterTex() {
+        return Assets.Environment.WATER_SEWERS;
+    }
 }
