8f2d46239a5207ccf5017fb43d7f7fa5e38743f5
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 15:08:20 +0200

    Create ShirdSprite.java

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ShirdSprite.java
@@ -0,0 +1,10 @@
+package com.shatteredpixel.shatteredpixeldungeon.sprites;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+
+public class ShirdSprite extends MobSprite {
+    public ShirdSprite() {
+        super();
+        texture = Assets.SHIRD; // Это твоя картинка shird.png
+    }
+}
