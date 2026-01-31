85999ff032c5ebb07cff5aba083890e07513c475
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 17:59:18 +0200

    Update Shird.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
@@ -6,6 +6,7 @@ import com.shatteredpixel.shatteredpixeldungeon.sprites.ShirdSprite;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
 
+// Важно: Shird должен наследоваться от Mob!
 public class Shird extends Mob {
 
     {
@@ -14,49 +15,43 @@ public class Shird extends Mob {
 
         hp = maxHP = 70;
         defenseSkill = 12;
-        // Скорость 1.2 делает его чуть быстрее обычных крыс
         baseSpeed = 1.2f;
 
-        // Это заставляет его сразу нападать, как только он увидит игрока
         state = HUNTING; 
     }
 
     @Override
     public int damageRoll() {
-        // Урон от 10 до 18
         return 10 + (int)(Math.random() * 9); 
     }
 
     @Override
     public int attackSkill(Char target) {
-        // Точность атаки
         return 15;
     }
 
     @Override
     public void notice() {
         super.notice();
-        // Босс кричит при обнаружении игрока
         yell("Я заберу твою душу этой лопатой!");
     }
 
     @Override
     public boolean attack(Char enemy) {
-        // Запускаем анимацию удара в спрайте
-        sprite.attack(enemy.pos);
+        // Проверяем, что спрайт существует перед вызовом анимации
+        if (sprite != null) sprite.attack(enemy.pos);
         return super.attack(enemy);
     }
 
     @Override
     public void die(Object cause) {
         super.die(cause);
-        // При смерти гарантированно выпадает твоя новая лопата
         Dungeon.level.drop(new Shovel(), pos).sprite.drop();
         GLog.w("Ширд повержен! Вы нашли его старую лопату.");
     }
 
     @Override
     public String description() {
-        return "Древний смотритель, сошедший с ума. Он не выпустит тебя живым.";
+        return "Древний смотритель, сошедший с ума.";
     }
 }
