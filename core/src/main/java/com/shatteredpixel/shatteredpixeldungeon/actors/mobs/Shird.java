9b7bf26ba31fc851bad87cb2f2a7eca06544f36b
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 18:51:27 +0200

    Update Shird.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
@@ -5,8 +5,8 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ShirdSprite;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
+import com.shatteredpixel.shatteredpixeldungeon.items.Amulet;
 
-// Обязательно "extends Mob", чтобы появились hp и name
 public class Shird extends Mob {
 
     {
@@ -14,35 +14,40 @@ public class Shird extends Mob {
         spriteClass = ShirdSprite.class;
 
         hp = maxHP = 1500; 
-        defenseSkill = 25;
-        baseSpeed = 1.0f;
+        defenseSkill = 30;
+        baseSpeed = 1f;
 
-        state = HUNTING; 
+        state = HUNTING;
     }
 
     @Override
     public int damageRoll() {
-        return 25 + (int)(Math.random() * 15); 
+        return 30 + (int)(Math.random() * 20);
     }
 
     @Override
     public int attackSkill(Char target) {
-        return 30;
+        return 35;
     }
 
-    // Убираем @Override attack, так как в Char он final. 
-    // Вместо этого используем стандартную логику моба.
-    
     @Override
     public void notice() {
         super.notice();
-        yell("Твой путь заканчивается здесь!");
+        yell("Конец близок... Амулет останется здесь!");
     }
 
     @Override
     public void die(Object cause) {
         super.die(cause);
+        
+        // 1. Выпадает лопата
         Dungeon.level.drop(new Shovel(), pos).sprite.drop();
-        GLog.w("Невероятно... Ширд пал. Вы получили его легендарную лопату!");
+        
+        // 2. Выпадает Амулет
+        Amulet amulet = new Amulet();
+        amulet.name = "Сердце Реалинга"; // Твоё название амулета
+        Dungeon.level.drop(amulet, pos).sprite.drop();
+        
+        GLog.w("Ширд повержен! Вы забрали Сердце Реалинга.");
     }
 }
