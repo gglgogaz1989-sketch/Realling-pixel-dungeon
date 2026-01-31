0aed2b29bd88c73c2d02073f53dfb0355772fb06
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 17:43:47 +0200

    Update Shovel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Shovel.java
@@ -1,8 +1,9 @@
-package com.shatteredpixel.shatteredpixeldungeon.items.weapons;
+package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
-import com.shatteredpixel.shatteredpixeldungeon.items.MeleeWeapon;
+// Проверь, нет ли буквы 's' в конце слова weapon в твоих папках!
+import com.shatteredpixel.shatteredpixeldungeon.items.MeleeWeapon; 
 import com.shatteredpixel.shatteredpixeldungeon.utils.Random;
 
 public class Shovel extends MeleeWeapon {
@@ -11,20 +12,16 @@ public class Shovel extends MeleeWeapon {
     }
 
     public Shovel() {
+        // Урон и скорость лопаты
         initValues(5, 12, 1.0f, 1.0f);
     }
 
     @Override
     public void proc(Char attacker, Char defender, int damage) {
         super.proc(attacker, defender, damage);
-        // Твоя идея с параличом 5% на 5 ходов
+        // Шанс 5% наложить паралич на 5 ходов
         if (Random.Float() < 0.05f) {
             Paralysis.affect(defender, 5f);
         }
-    }
-
-    @Override
-    public String desc() {
-        return "Старая садовая лопата. Удар может парализовать цель.";
     }
 }
