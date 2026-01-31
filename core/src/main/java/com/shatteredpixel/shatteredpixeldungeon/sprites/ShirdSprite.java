c2d387def58751e562397fe3b3add0bfc9a64030
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 15:18:42 +0200

    Update ShirdSprite.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ShirdSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ShirdSprite.java
@@ -1,10 +1,32 @@
 package com.shatteredpixel.shatteredpixeldungeon.sprites;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Shird;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 
 public class ShirdSprite extends MobSprite {
-    public ShirdSprite() {
-        super();
-        texture = Assets.SHIRD; // Это твоя картинка shird.png
+
+    @Override
+    public void link(Mob mob) {
+        super.link(mob);
+        // По умолчанию ставим спящую текстуру
+        texture = Assets.SHIRD_SLEEP; 
     }
-}
+
+    @Override
+    public void update() {
+        super.update();
+        if (ch == null) return;
+
+        // Логика смены картинок по стадиям ХП
+        if (ch.HP <= 0) {
+             // Текстура смерти (можно использовать idle)
+        } else if (((Shird)ch).isCasting) {
+            texture = Assets.SHIRD_CAST; // Стадия призыва
+        } else if (ch.state == ch.WANDERING || ch.state == ch.HUNTING) {
+            texture = Assets.SHIRD_IDLE; // Проснулся и стоит/ходит
+        } else {
+            texture = Assets.SHIRD_SLEEP; // Спит
+        }
+    }
+    }
