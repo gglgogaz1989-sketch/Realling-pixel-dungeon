7297ce142a03fc7b86e86f06cdd4ded23c2d52c9
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 17:55:51 +0200

    Update Shird.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Shird.java
@@ -4,7 +4,7 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ShirdSprite;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
-import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Shovel;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shovel;
 
 public class Shird extends Mob {
 
@@ -12,40 +12,51 @@ public class Shird extends Mob {
         name = "Ширд";
         spriteClass = ShirdSprite.class;
 
-        hp = maxHP = 60;
-        defenseSkill = 10;
+        hp = maxHP = 70;
+        defenseSkill = 12;
+        // Скорость 1.2 делает его чуть быстрее обычных крыс
         baseSpeed = 1.2f;
 
-        // Вместо MeleeAI используем стандартное состояние охоты
+        // Это заставляет его сразу нападать, как только он увидит игрока
         state = HUNTING; 
     }
 
     @Override
     public int damageRoll() {
-        return 8 + (int)(Math.random() * 7); 
+        // Урон от 10 до 18
+        return 10 + (int)(Math.random() * 9); 
     }
 
     @Override
     public int attackSkill(Char target) {
-        return 14;
+        // Точность атаки
+        return 15;
     }
 
     @Override
     public void notice() {
         super.notice();
-        yell("Кто посмел потревожить мои владения?!");
+        // Босс кричит при обнаружении игрока
+        yell("Я заберу твою душу этой лопатой!");
+    }
+
+    @Override
+    public boolean attack(Char enemy) {
+        // Запускаем анимацию удара в спрайте
+        sprite.attack(enemy.pos);
+        return super.attack(enemy);
     }
 
     @Override
     public void die(Object cause) {
         super.die(cause);
-        // Выпадение лопаты при смерти
+        // При смерти гарантированно выпадает твоя новая лопата
         Dungeon.level.drop(new Shovel(), pos).sprite.drop();
-        GLog.w("Ширд повержен! Лопата выпала на землю.");
+        GLog.w("Ширд повержен! Вы нашли его старую лопату.");
     }
 
     @Override
     public String description() {
-        return "Древний страж канализации. Он выглядит очень злым.";
+        return "Древний смотритель, сошедший с ума. Он не выпустит тебя живым.";
     }
 }
